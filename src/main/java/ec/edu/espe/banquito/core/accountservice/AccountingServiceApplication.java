998030b7c85c7f71da974d8e9001a80ae36c1b7e
alanherrera01 <avherrera3@espe.edu.ec>
2026-07-16 10:47:02 -0500

    test backend ci cd log marker

--- src/main/java/ec/edu/espe/banquito/core/accountservice/AccountingServiceApplication.java
+++ src/main/java/ec/edu/espe/banquito/core/accountservice/AccountingServiceApplication.java
@@ -8,6 +8,7 @@ public class AccountingServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountingServiceApplication.class, args);
+		System.out.println("CI/CD backend validation: accounting-service started");
 	}
 
 }
